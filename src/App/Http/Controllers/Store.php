a7a4ada0345fcc1f458a70ea74f5bca2bbbc4997
Sergiu Popa <popa.sergiu920@gmail.com>
2020-05-12 10:41:19 +0300

    create new avatar if not exist

--- src/App/Http/Controllers/Store.php
+++ src/App/Http/Controllers/Store.php
@@ -5,13 +5,14 @@ namespace LaravelMerax\Avatars\App\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use LaravelMerax\Avatars\App\Http\Requests\ValidateAvatarRequest;
+use LaravelMerax\Avatars\App\Models\Avatar;
 
 class Store extends Controller
 {
 
     public function __invoke(ValidateAvatarRequest $request)
     {
-        $avatar = $request->user()->avatar;
+        $avatar = $request->user()->avatar ?? new Avatar();
         $avatar->store($request->file('file'));
 
         return response()->json([
